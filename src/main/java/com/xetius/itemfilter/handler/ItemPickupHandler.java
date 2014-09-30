ebe8ab63a68db1ee7506e4d821c0d7c66716906b
Chris Hudson <chris.hudson@digitaslbi.com>
2014-09-30 12:42:57 +0100

    Committed mcmod.info file.  Required for Config/Disable Mod Option buttons

--- src/main/java/com/xetius/itemfilter/handler/ItemPickupHandler.java
+++ src/main/java/com/xetius/itemfilter/handler/ItemPickupHandler.java
@@ -2,11 +2,20 @@ package com.xetius.itemfilter.handler;
 
 import com.xetius.itemfilter.utility.LogHelper;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
+import net.minecraft.init.Items;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 
 public class ItemPickupHandler {
     @SubscribeEvent
     public void onItemPickup(EntityItemPickupEvent event) {
-        LogHelper.info("Item picked up" + event.toString());
+        ItemStack itemStack = event.item.getEntityItem();
+
+        LogHelper.info("Item picked up" + itemStack + " (" + itemStack.getItem() + ")");
+
+        if (itemStack.getItem().equals(Items.wheat_seeds)) {
+            event.setCanceled(true);
+            LogHelper.info("Not picking up seeds");
+        }
     }
 }
